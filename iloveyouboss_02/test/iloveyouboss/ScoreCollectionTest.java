c4fe0498d9ae38f734ebd0c4b7cbbb84ac273a14
knecht <ldj910505@naver.com>
2022-08-22 01:40:50 +0900

    초기 파일 업로드

---
+++ iloveyouboss_02/test/iloveyouboss/ScoreCollectionTest.java
@@ -0,0 +1,21 @@
+/***
+ * Excerpted from "Pragmatic Unit Testing in Java with JUnit",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/utj2 for more book information.
+***/
+package iloveyouboss;
+
+import static org.junit.Assert.*;  //(1)
+import org.junit.*;                //(2)
+
+public class ScoreCollectionTest { // (3) 많은 부분은 타겟 클래스 이름에 Test를 붙여서 테스트 클래스 이름을 만든다.
+
+	@Test                           // (4)
+	public void test() {            // (5)
+		fail("Not yet implemented"); // (6)
+	}
+
+}
